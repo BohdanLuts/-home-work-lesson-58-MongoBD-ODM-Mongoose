2c7c33c8cd88fbe521650e684449fdab7b866ec8
Viktoriia <pecourses>
2023-04-30 17:27:45 +0300

    Add get use, get user byId

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,7 +8,7 @@ module.exports.createUser = async (req, res, next) => {
     const createdUser = await User.create(body);
 
     if (!createdUser) {
-      createHttpError(400, 'Bad Request');
+      return next(createHttpError(400, 'Bad Request'));
     }
 
     res.status(201).send({ data: createdUser });
@@ -17,9 +17,32 @@ module.exports.createUser = async (req, res, next) => {
   }
 };
 
-module.exports.getUsers = async (req, res, next) => {};
+module.exports.getUsers = async (req, res, next) => {
+  const { limit = 10 } = req.query;
 
-module.exports.getUserById = async (req, res, next) => {};
+  try {
+    const foundUser = await User.find().sort({ _id: 1 }).limit(limit).skip();
+
+    res.status(200).send({ data: foundUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getUserById = async (req, res, next) => {
+  const { userId } = req.params;
+
+  try {
+    const foundUser = await User.findById(userId);
+    if (!foundUser) {
+      return next(createHttpError(404, 'User Not Found'));
+    }
+
+    res.status(200).send({ data: foundUser });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports.updateUserById = async (req, res, next) => {};
 
