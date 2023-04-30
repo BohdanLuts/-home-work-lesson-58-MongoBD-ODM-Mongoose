bce547cab225134cc82b4f1ccfcbf733312ff557
Viktoriia <pecourses>
2023-04-30 15:46:12 +0300

    Add mongodb connection

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const env = process.env.NODE_ENV ?? 'development';
+const { host, port, dbName } = require('./../config/mongoDBConfigs')[env];
+
+mongoose
+  .connect(`mongodb://${host}:${port}/${dbName}`)
+  .then(() => console.log('Connection OK'))
+  .catch(err => console.log('err :>> ', err));
